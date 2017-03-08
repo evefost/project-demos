f3e6fb2457f1860c129eabb475cdfb96220eaf4b
xieyang <450463362@qq.com>
2017-03-09 02:07:38 +0800

    替换数据驱动

--- standard-project/standard-web/src/main/java/com/xie/java/controller/RestFulDesignController.java
+++ standard-project/standard-web/src/main/java/com/xie/java/controller/RestFulDesignController.java
@@ -4,8 +4,10 @@ import com.xie.java.common.response.ResponseDataVo;
 import com.xie.java.entity.TestBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
 
