45abb4facc5bcf274657a2fd9b2871906f1311ff
xieyang <450463362@qq.com>
2017-03-08 17:23:19 +0800

    restful api demo

--- standard-project/standard-web/src/main/java/com/xie/java/controller/RestFulDesignController.java
+++ standard-project/standard-web/src/main/java/com/xie/java/controller/RestFulDesignController.java
@@ -5,24 +5,20 @@ import com.xie.java.entity.TestBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Random;
 
 /**
  * restful风格接口
  */
-@Controller
+@RestController
 @RequestMapping("/restful")
 public class RestFulDesignController {
     private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
 
     @RequestMapping(value = "/user", method = RequestMethod.POST)
-    @ResponseBody
     public ResponseDataVo addUser() {
         logger.info("添加用户");
         TestBean testBean = new TestBean();
@@ -33,7 +29,6 @@ public class RestFulDesignController {
 
     //http://127.0.0.1:8080/restful/user/11
     @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
-    @ResponseBody
     public ResponseDataVo getUser(@PathVariable Long id) {
         logger.info("获取用户id:" + id);
 
@@ -41,17 +36,15 @@ public class RestFulDesignController {
     }
 
     @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
-    @ResponseBody
     public ResponseDataVo deleteUser(@PathVariable Long id) {
         logger.info("删除用户id:" + id);
 
         return ResponseDataVo.success(null, "删除用户");
     }
 
-    @RequestMapping(value = "/user/{id}", method = RequestMethod.PUT)
-    @ResponseBody
-    public ResponseDataVo updateUser(@PathVariable Long id) {
-        logger.info("更新用户:" + id);
+    @RequestMapping(value = "/user/{id}/{name}", method = RequestMethod.PUT)
+    public ResponseDataVo updateUser(@PathVariable Long id,@PathVariable String name) {
+        logger.info("更新用户:" + id+"=="+name );
         return ResponseDataVo.success(null, "更新用户");
     }
 
