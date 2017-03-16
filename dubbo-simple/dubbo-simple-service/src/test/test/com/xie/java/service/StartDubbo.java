39ab4bd730b54c299364488a359b2484dc0e0a77
xieyang <450463362@qq.com>
2017-03-17 05:12:59 +0800

    升级mybatisplus

---
+++ dubbo-simple/dubbo-simple-service/src/test/test/com/xie/java/service/StartDubbo.java
@@ -0,0 +1,13 @@
+package test.com.xie.java.service;
+
+import com.alibaba.dubbo.container.Main;
+
+/**
+ * Created by xieyang on 17/3/16.
+ */
+public class StartDubbo {
+
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
